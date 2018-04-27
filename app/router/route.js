cb76537039834cfb92d6e31d487b47126c03234a
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-04-27 23:38:19 +0300

    server is up and running. dependencies added.

--- app/router/route.js
+++ app/router/route.js
@@ -2,7 +2,12 @@ const express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("yey!");
+    res.render('index');
+});
+
+router.get('/new/:link', (req, res) => {
+    let link = req.params.link;
+    res.send(link);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const router = require('./app/router/route.js');
+const path = require('path');
 const PORT = process.env.PORT || 8427;
 var app = express();
 
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 app.use(router);
 app.listen(PORT, () => {
     console.log("Server is listening on " + PORT);
