0a2244d530766537be8e8724776a026df949d3f6
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-04-29 03:14:05 +0300

    router for accessing shortened urls added.

--- app/route.js
+++ app/route.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const shortener = require('./shortener.js');
+const redirector = require('./redirector.js');
 var router = express.Router();
 
+// TODO: add db disconnect functionality when client disconnects
+// TODO: ?v= gibi url'de parametre barindiran url'leri almiyor.
+// TODO: yeni bir link yaratildiginda sonuclari gosterirken _id nerden geliyor amk?
+// TODO: drop the collection when everything is ready and re-create it
+
+// main page router
 router.get('/', (req, res) => {
     res.render('index');
 });
 
-router.get('/new/:link', async (req, res) => {
-    let link = req.params.link;
-    console.log("link: ", link);
-    let results = await shortener(link);
+// router for redirecting short urls to their original urls
+router.get('/:shortLink', async (req, res) => {
+    let websiteUrl = req.protocol + '://' + req.get('host') + "/";
+    let shortLink = websiteUrl + req.params.shortLink;
+    let originalLink = await redirector(shortLink);
+    if(originalLink !== null)
+        res.redirect(originalLink[0]["original_url"]);
+    else
+        res.json({"error": "This url is not on the database"});
+});
+
+// router for creating short urls
+router.get('/new/:originalLink(*)', async (req, res) => {
+    let websiteUrl = req.protocol + '://' + req.get('host') + "/";
+    let originalLink = req.params.originalLink;
+    let results = await shortener(originalLink, websiteUrl);
+    console.log(results);
     res.json(results);
 });
 
