7c9c2121acc771354808ac8d2d65ad92ec958c3c
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-04-29 03:14:55 +0300

    db controller for retrieving original link of given shortened links added

--- app/dbController.js
+++ app/dbController.js
@@ -1,38 +1,43 @@
 const db = require('./db.js');
 let dbObj;
 
+// create an instance of database at start
 db.connectDB((err) => {
     if(err) throw err;
     dbObj = db.getDBObj();
 });
 
-function updateDB(result) {
-    db.connectDB((err) => {
+function insertShortLink(result) {
+    // inserts new original-short link pair to the database
+    let urls = dbObj.collection('urls');
+    urls.insert(result, (err, data) => {
         if(err) throw err;
-        let urls = dbObj.collection('urls');
-        // Insert the results to the DB
-        urls.insert(result, (err, data) => {
-            if(err) throw err;
-            console.log("Inserted!");
-            db.disconnectDB();
-        })
     })
 }
 
 function shortened(url) {
     return new Promise(resolve => {
-        console.log("shortened?");
         let query = { "original_url": url };
         let projection = { _id: false, "original_url": true, "short_url": true };
         let cursor = dbObj.collection('urls').find(query).project(projection);
         cursor.toArray((err, documents) => {
-            console.log("documents: ", documents);
-            if(documents.length > 0)
+            if(documents !== null && documents.length > 0)
                 resolve(documents[0]);
             else
                 resolve(false);
-        })
-    })
+        });
+    });
 }
 
-module.exports = { updateDB, shortened }
+function getOriginalLink(shortLink) {
+    return new Promise(resolve => {
+        let query = { "short_url": shortLink };
+        let projection = { _id: false, "original_url": true };
+        let cursor = dbObj.collection('urls').find(query).project(projection);
+        cursor.toArray((err, documents) => {
+            resolve(documents);
+        });
+    });
+}
+
+module.exports = { insertShortLink, shortened, getOriginalLink }
